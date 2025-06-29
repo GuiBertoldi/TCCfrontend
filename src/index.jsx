64c91da97ee78e48161c4f34bee9b7e2f2907480
GuiBertoldi <guilherme_chuia@hotmail.com>
2025-06-29 01:57:01 -0300

    teste

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,9 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
-
-import { datadogRum } from '@datadog/browser-rum'
-import { reactPlugin } from '@datadog/browser-rum-react'
-
-datadogRum.init({
-  applicationId: process.env.REACT_APP_DD_RUM_APP_ID,
-  clientToken:   process.env.REACT_APP_DD_RUM_CLIENT_TOKEN,
-  site:          'datadoghq.com',
-  service:       process.env.REACT_APP_DD_RUM_SERVICE,
-  env:           process.env.REACT_APP_ENV,
-  version:       process.env.REACT_APP_VERSION,
-  sessionSampleRate:       100,
-  sessionReplaySampleRate: 20,
-  defaultPrivacyLevel:     'mask-user-input',
-  plugins: [reactPlugin({ router: true }) ],
-  debug: true,
-  intakeUrls: {
-    rum:           'https://rum-http-intake.logs.datadoghq.com/v1/input',
-    sessionReplay: 'https://session-replay-http-intake.logs.datadoghq.com/v1/input'
-  }
-})
-
-window.datadogRum = datadogRum
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
